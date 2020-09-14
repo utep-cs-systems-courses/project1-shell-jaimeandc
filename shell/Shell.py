d0f27675b6081cd5141051c24b26d368a3f3298f
Carlos A Jaime <cajaime@miners.utep.edu>
2020-09-13 21:22:29 -0600

    Redirect Functional

--- shell/Shell.py
+++ shell/Shell.py
@@ -1,23 +1,50 @@
 #! /usr/bin/env python3
 import os, sys, re, time
-
-def command_check(command):
-    part = command.split()
-    if len(part) > 2:
-        return False
-    else:
-        return True
     
-while True:
-    command = input("$ ")
-    if command == "quit":
-        sys.exit(1)
-    elif command == "help":
-        print("\t\tWelcome to Help\n\tFormat:<cmd><arg>\n\tQuit:'quit'")
+def pipe_check(command):
+    cmd = re.findall('[#:|]',command)
+    if cmd:
+        return True
     else:
-        if command_check(command) == False:
-            os.write(1,("WRONG FORMAT").encode())
-        else: 
+        return False
+
+while True:
+    command = input("$ ")  ## Prompt the user to enter Command
+    if pipe_check(command) == True:## Check users command for pipe command
+        os.write(1,("YOU WANNA PIPE BUT WE CANT DO THAT JUST YET\n").encode())
+    
+    if command == "quit":  ## Exit Program
+         sys.exit(0)
+         
+    elif command == "help":## Enter help Menu
+        print("\t\tWelcome to Help\n\tFormat:<cmd><args>\n\tQuit:'quit'")
+
+    elif command == "clear history":
+        open('history.txt', 'w').close()
+
+    elif command == "redirect": ## REDIRECT
+        command = input("$ ")
+        rc = os.fork()  ## Create Child to Run Process
+        if rc < 0: ##If Child Failed
+            os.write(2,("Forked Failed, Returning %d\n" % rc).encode())
+            sys.exit(1)
+        elif rc == 0: ## If Child is successful 
+            args = command.split()
+            os.close(1) ##Redirect output to write to out.txt
+            os.open("out.txt", os.O_WRONLY | os.O_APPEND);
+            os.set_inheritable(1, True)
+            
+            for dir in re.split(":", os.environ['PATH']):
+                program = "%s/%s" % (dir, args[0]) ##
+                try:
+                    os.execve(program,args,os.environ)
+                except FileNotFoundError:
+                    pass
+                
+            os.write(2,("Child : Error Could not exec %s\n" % args[1]).encode())
+            sys.exit(0)
+        
+    else:
             rc = os.fork()
             if rc < 0:
                 os.write(2,("Fork Failed, returning %d\n" % rc).encode())
@@ -26,7 +53,7 @@ while True:
                 args = command.split()
                 for dir in re.split(":", os.environ['PATH']):
                     program = "%s/%s" % (dir, args[0])
-                    os.write(1,("Child is doing work...\n").encode())
+                    os.write(1,("Trying to exec %s\n" % args[0]).encode())
                     try:
                         os.execve(program, args, os.environ)
                         break
@@ -36,11 +63,4 @@ while True:
                 os.write(1, ("Child:  Could not exec %s\n" % args[0]).encode())
                 sys.exit(1)
                 
-            else:
-                time.sleep(1)
-                print("Child is done... Back to parent")
-            
-                
-            
-            
             
