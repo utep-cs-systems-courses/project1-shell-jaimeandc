09761acfc53e41633a5348886a62aa2555e71115
Carlos A Jaime <cajaime@miners.utep.edu>
2020-09-19 20:59:59 -0600

    Input redirect fixed and testLog added

--- shell/Shell.py
+++ shell/Shell.py
@@ -2,42 +2,30 @@
 import os, sys, re, time
     
 
-def re_in(command):
-    file_index = command.index('<') - 1
-    filename = command[file_index]
-    cmd_index = command.index('<') +1
-    args = command[cmd_index:]
+def re_in(command): # Forks Child and attempt to Redirect Input
     pid = os.getpid()
     rc = os.fork()
     if rc < 0:
         os.write(2,("Fork Failed, Returning").encode())
         sys.exit(1)
     elif rc == 0:
-        os.close(1)
-        sys.stdout = open(filename,"w")
-        os.set_inheritable(1,True)
-        try:
-            os.execve(command[1],command,os.environ)
-        except FileNotFoundError:
-            pass
+        os.close(0)
+        os.open(command[command.index('<')+1], os.O_RDONLY)
+        os.set_inheritable(0,True)
             
         for dir in re.split(":", os.environ['PATH']):
-            program = "%s/%s" % (dir, args[0])
+            program = "%s/%s" % (dir, command[0])
             try:
-                os.execve(program,args,os.environ)
+                os.execve(program,command,os.environ)
             except FileNotFoundError:
                 pass
-        os.write(1,("Could not exec: %s\n"%args[0]).encode())
+        os.write(1,("Could not exec: %s\n"%command[0]).encode())
         sys.exit(0)
     else:
         childPid = os.wait()
                 
     
-def re_out(command):
-    file_index = command.index('>') + 1
-    cmd_index = command.index('>')
-    filename = command[file_index]
-    args = command[:cmd_index]
+def re_out(command):## Fork child and attempt to Redirect Output
     pid = os.getpid()
     rc = os.fork()
     if rc < 0:
@@ -45,28 +33,27 @@ def re_out(command):
         sys.exit(1)
     elif rc == 0:
         os.close(1)
-        sys.stdout = open(filename, "w")
+        os.open(filename, os.O_CREAT | os.O_WRONLY)
         os.set_inheritable(1,True)
 
         for dir in re.split(":", os.environ['PATH']):
-            program = "%s/%s" % (dir, args[0])
+            program = "%s/%s" % (dir, command[0])
             try:
-                os.execve(program, args, os.environ)
+                os.execve(program,command, os.environ)
             except FileNotFoundError:
                 pass
-        os.write(1,("Could not exec: %s\n" % args[0]).encode())
+        os.write(1,("Could not exec: %s\n" % command[0]).encode())
         sys.exit(0)
     else:
         childPid = os.wait()
         
-def run_command(command):
+def run_command(command):## Fork child to attemp to run command, handle if cmd is not valid
     pid = os.getpid()
     rc = os.fork()
     if rc < 0:
         os.write(2,("Fork Failed").encode())
         sys.exit(1)
     elif rc == 0:
-        
         for dir in re.split(":", os.environ['PATH']):
             program = "%s/%s" % (dir, command[0])
             try:
@@ -94,11 +81,10 @@ def pipe(args):
         sys.exit(1)
     elif rc == 0:
         args = args[:pipe]
-        
-        os.close(1)
-        
+        os.close(1) 
         fd = os.dup(pw)
         os.set_inheritable(fd, True)
+        
         for fd in (pr, pw):
             os.close(fd)
             
@@ -113,12 +99,10 @@ def pipe(args):
             
     else:
         args = args[pipe+1:]
-        
         os.close(0)
-        
         fd = os.dup(pr)
         os.set_inheritable(fd, True)
-        
+    
         for fd in (pw, pr):
             os.close(fd)
             
@@ -138,27 +122,32 @@ def pipe(args):
             os.write(2,("%s command not found"%args[0]).encode())
             sys.exit(1)
 
-def input_handler(command):
+def input_handler(command): ## Checks user input for special "words" or cmd characters
     command = command.split()
-    if 'exit' in command:
+    if 'exit' in command: ##Exit program
         sys.exit(0)
-    elif not command:
+    elif not command: ##handles if input is empty
         pass
-    elif '<' in command:
+    elif '<' in command:##Checks input for "input redirect" 
         re_in(command)
-    elif '>' in command:
+    elif '>' in command:## Checks input for "output redirect"
         re_out(command)
-    elif '|' in command:
+    elif '|' in command:## Checks input for "Pipe" command
         args = command
         pipe(args)
-    elif 'cd' in command:
+
+    elif '&' in command:
+        command.remove("&")
+        wait = False
+    elif 'cd' in command:## Handles Change Directory command
         directory = command[1]
         try:
             os.chdir(directory)
         except FileNotFoundError:
             os.write(2,("File: %s not found"%directory).encode())
-    else:
+    else: ## if no special command is found attempt to run command
         run_command(command)
+        
 
 while True:
     if 'PS1' in os.environ:
@@ -173,4 +162,4 @@ while True:
     except ValueError:
         sys.exit(1)
             
-    input_handler(user_cmd)
+    input_handler(user_cmd) ##Takes user input to handler 
