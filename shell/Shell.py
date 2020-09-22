a46d1b39a8e546807fd424bd14807637c3eb1c80
Carlos A Jaime <cajaime@miners.utep.edu>
2020-09-21 18:34:29 -0600

    input() changed to os.read()

--- shell/Shell.py
+++ shell/Shell.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python3
-import os, sys, re, time
-    
+import os, sys, re
+from aifc import Error
+
+class NoArgumentsError(Error):
+    pass
+class TooManyArgumentsError(Error):
+    pass
 
 def re_in(command): # Forks Child and attempt to Redirect Input
     pid = os.getpid()
@@ -123,43 +128,75 @@ def pipe(args):
             sys.exit(1)
 
 def input_handler(command): ## Checks user input for special "words" or cmd characters
-    command = command.split()
-    if 'exit' in command: ##Exit program
+    if len(command) == 0: ##If empty command continue with prompt
+        return
+    elif command[0] == "exit":##Exit Program
         sys.exit(0)
-    elif not command: ##handles if input is empty
-        pass
-    elif '<' in command:##Checks input for "input redirect" 
-        re_in(command)
-    elif '>' in command:## Checks input for "output redirect"
-        re_out(command)
-    elif '|' in command:## Checks input for "Pipe" command
-        args = command
-        pipe(args)
-
-    elif '&' in command:
-        command.remove("&")
-        wait = False
-    elif 'cd' in command:## Handles Change Directory command
-        directory = command[1]
+    elif command[0] == "cd":##Check for change directory cmd
         try:
-            os.chdir(directory)
+            if len(command) < 2:## Handle if user dosnt enter arguments
+                raise NoArgumentsError
+            elif len(command) > 2:## Handle if user enters noo many arguments
+                raise TooManyArgumentsError
+            else:
+                os.chdir(command[1])
+        except NoArgumentsError:
+            os.write(2,"No Directory Provided\n".encode())
+        except TooManyArgumentsError:
+            os.write(2,"Too Many Arguments\n".encode())
         except FileNotFoundError:
-            os.write(2,("File: %s not found"%directory).encode())
-    else: ## if no special command is found attempt to run command
-        run_command(command)
-        
+            os.write(2,"Directory %s not found\n"%command[1].encode())
 
-while True:
-    if 'PS1' in os.environ:
-        os.write(1,(os.environ['PS1']).encode())
+    elif "|" in command:##Handle if user enters "Pipe" cmd
+        pipe(command)
+    elif ">" in command:##Handle if user wants to redirect output
+        re_out(command)
+    elif "<" in command:##Handle if user wants to redirect input
+        re_in(command)
     else:
-        os.write(1,("$ ").encode())
-        
-    try:
-        user_cmd = input()
-    except EOFError:
-        sys.exit(1)
-    except ValueError:
-        sys.exit(1)
-            
-    input_handler(user_cmd) ##Takes user input to handler 
+        rc = os.fork()
+        wait = True
+
+        if "&" in command: ## Check for background command
+            command.remove("&")
+            wait = False
+        if rc < 0:## Fork Failed
+            sys.exit(1)
+        elif rc == 0:
+            run_command(command)
+            sys.exit(0)
+        else:
+            if wait:
+                result = os.wait()
+                if result[1] != 0 and result[1] != 256:
+                    os.write(2,("Program terminated with exit code: %d\n"%result[1]).encode())
+
+                    
+def get_args():
+    args = os.read(0,128)
+    return args
+
+
+
+def shell():
+    while True:
+        prompt = "$ "
+        if "PS1" in os.environ:
+            prompt = os.environ["PS1"]
+
+        os.write(1, prompt.encode())
+        command = get_args()
+
+        if len(command) == 0:
+                break
+    
+        command = command.decode().split("\n")
+    
+        if not command:
+            continue
+    
+        for cmd in command:
+            input_handler(cmd.split())
+    
+if __name__ == "__main__":
+    shell()
